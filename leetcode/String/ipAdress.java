7e47ddb08d3b0554075b2631242bc38b478f540c
hitesh09saini <hitesh9694saini@gmail.com>
2023-10-08 12:21:01 +0530

    d

---
+++ leetcode/String/ipAdress.java
@@ -0,0 +1,18 @@
+package leetcode.String;
+
+public class ipAdress {
+    public static void main(String[] args) {
+        String s = "1.1.1.1";
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '.') {
+                sb.insert(i, '[');
+                sb.insert(i + 2, ']');
+                i += 2; 
+            }
+        }
+        System.out.println('1'-'3');
+
+       
+    }
+}
